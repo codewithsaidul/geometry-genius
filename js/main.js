0cecbc9bfc99479b8dfeaaf04ec70f2e6a44bd9f
codewithsaidul <codewithsaidul@gmail.com>
2024-02-12 22:48:58 +0600

    Rectangle Area Added

--- js/main.js
+++ js/main.js
@@ -1,18 +1,59 @@
+
+// Get The Input From Input Field
+function getInput(inputById) {
+  const inputField = document.getElementById(inputById);
+  const inputFieldText = inputField.value;
+  const value = parseFloat(inputFieldText);
+
+
+  inputField.value = '';
+  return value;
+}
+
+
+// Get The Area Result Zone
+function elementResult(elementId, area) {
+  const elementArea = document.getElementById(elementId);
+  elementArea.innerText = area;
+  return elementArea;
+}
+
+
+
+// Calculate Triangle Area
 function calculateTriangleArea() {
   // Get Triangle Based Value
-  const triangleBaseInput = document.getElementById("triangle-base");
-  const triangleBased = parseFloat(triangleBaseInput.value);
+  const triangleBase = getInput("triangle-base");
 
   // Get The Triangle Height Value
-  const triangleHeightInput = document.getElementById("triangle-height");
-  const triangleHeight = parseFloat(triangleHeightInput.value);
+  const triangleHeight = getInput("triangle-height");
 
   // Calculate The Triangle Area
-  const area = 0.5 * triangleBased * triangleHeight;
+  const triangleArea = 0.5 * triangleBase * triangleHeight;
 
-  // Display Triangle Area; 
-  const triangleAreaDisplay = document.getElementById('triangle-area')
-  triangleAreaDisplay.innerText = area;
+  // Display Triangle Area;
+  const reulstArea = elementResult("triangle-area", triangleArea);
 
-  return triangleAreaDisplay;
+  // Return The Result
+  return reulstArea;
+}
+
+
+
+// Calculate Rectangle Area
+function calculateRectangleArea() {
+  // Get The Rectangle Width
+  const rectangleWidth = getInput("rectangle-width");
+
+  // Get The Rectangle Height
+  const rectangleheight = getInput("rectangle-height");
+
+  // Get The Rectangle Area
+  const rectangleArea = rectangleWidth * rectangleheight;
+
+  // Display The Rectangle Area Result
+  const area = elementResult("rectangle-area", rectangleArea);
+
+  // Return the Result
+  return area;
 }
